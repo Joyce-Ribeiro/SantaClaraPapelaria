46b366409034c449ba87af5b8fe8054d8d581310
Joyce-Ribeiro <joyribeirogxavier@gmai.com>
2025-03-23 15:56:39 -0300

    feat(Swagger): view vendedor

--- santaclara/urls.py
+++ santaclara/urls.py
@@ -1,21 +1,20 @@
-"""santaclara URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Configuração do Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Santa Clara",
+        default_version="v1",
+        description="API para gestão de cadastro e vendas",
+    ),
+    public=True,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # Rota para o painel admin
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),  # Rota para o Swagger
+    path('', include('cadastro.urls')),  # Incluindo as URLs do módulo de cadastro
 ]
